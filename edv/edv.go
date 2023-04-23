d87afe2bf0b241f2d3e56fe277930d5bbbfb5154
Vladan <vladan.andjelkovic@gmail.com>
2023-04-23 22:38:57 +0200

    solaris edit from ed

--- edv/edv.go
+++ edv/edv.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World and Solaris 3 ed!!")
+    fmt.Println("Hello, World and Solaris 4 ed!!")
     fmt.Println("Test in fedora VSC 4")
 }
